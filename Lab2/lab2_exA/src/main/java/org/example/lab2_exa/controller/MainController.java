d9f131418fc6e5174d082d78be0842bbb1e0e272
7Fin7 <finley.leggett@gmail.com>
2024-10-30 20:25:40 +0000

    Lab2 completed

--- Lab2/lab2_exA/src/main/java/org/example/lab2_exa/controller/MainController.java
+++ Lab2/lab2_exA/src/main/java/org/example/lab2_exa/controller/MainController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    // This annotation maps the HTTP request to the "/greet" URL to this method
     @RequestMapping("/greet")
     public String greetWorld(Model model) {
+        // Adds an attribute "name" with the value "World" to the model
         model.addAttribute("name", "World");
+
+        // Returns the name of the view to be rendered (in this case, "greeting")
         return "greeting";
     }
 }
--- Lab2/lab2_exB/src/main/java/org/example/lab2_exb/controller/DateController.java
+++ Lab2/lab2_exB/src/main/java/org/example/lab2_exb/controller/DateController.java
@@ -10,18 +10,20 @@ import java.util.Date;
 @Controller
 public class DateController {
 
+    // This annotation maps the HTTP request to the "/whatDate" URL to this method
     @RequestMapping("/whatDate")
     public String date(Model model) {
 
-        // create SimpleDateFormat object
+        // Create SimpleDateFormat object
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        Date currentDate = new Date();  // create Date object with current date
-        String dateString = sdf.format(currentDate);   // format current date to 'dd/MM/yyy'
+        Date currentDate = new Date();  // Create Date object with current date
+        String dateString = sdf.format(currentDate);   // Format current date to 'dd/MM/yyy'
 
         // Add the formatted date to the model
         model.addAttribute("currentDate", dateString);
 
+        // Returns the name of the view to be rendered (in this case, "showDate")
         return "showDate";
     }
 }
